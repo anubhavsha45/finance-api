5de0a14a5effbd40a12c901ecc167cb318920223
anubhavsha45 <anubhavsharma0719@gmail.com>
2026-04-02 23:21:03 +0530

    Getting this API ready for deployment

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const globalErrorController = require("./controllers/errorController");
 const userRoutes = require("./routes/userRoutes");
 const recordRoutes = require("./routes/recordRoutes");
 const rateLimit = require("express-rate-limit");
-const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 
 //HELMET
@@ -13,9 +12,6 @@ app.use(helmet());
 
 //bodyparser
 app.use(express.json());
-
-//DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
-app.use(mongoSanitize());
 
 //LIMIT REQUEST FROM THE SAME IP
 const limiter = rateLimit({
